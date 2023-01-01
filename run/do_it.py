2c352312e509b50eb3fe8515b07ceced18666887
RaikoPipe <eclipsedust@gmail.com>
2023-01-01 17:48:52 +0100

    - progress on neo integration

--- run/do_it.py
+++ run/do_it.py
@@ -11,7 +11,7 @@ import panda_gym
 from algorithms.SAC_hybrid.prior_controller_neo import RRMC
 
 
-def evaluate(prior,  num_steps=10000):
+def evaluate(prior,  num_steps=20000):
     """
     Evaluate a RL agent
     :param prior: (BaseRLModel object) the RL Agent
@@ -21,24 +21,28 @@ def evaluate(prior,  num_steps=10000):
 
     episode_rewards = [0.0]
     obs, _ = env.reset()
+    done_events = []
     for i in range(num_steps):
         # _states are only useful when using LSTM policies
         action = prior.compute_action_neo(env.task.goal)# [0.07996564, -0.13340622, 0.02173809])
         #rl_action, _ = model.predict(obs)
 
         obs, reward, done, truncated, info, = env.step(action)
-        sleep(0.01)
+        #sleep(0.01)
 
         # Stats
         episode_rewards[-1] += reward
         if done or truncated:
-            sleep(0.5)
+            # sleep(0.5)
             if info["is_success"]:
                 print("Success!")
+                done_events.append(1)
             elif info["is_truncated"]:
                 print("Collision...")
+                done_events.append(-1)
             else:
                 print("Timeout...")
+                done_events.append(0)
             obs, _ = env.reset()
             # unnecessary step (?) reset panda
             prior.panda_rtb.q = prior.panda_rtb.qr
@@ -46,6 +50,8 @@ def evaluate(prior,  num_steps=10000):
     # Compute mean reward for the last 100 episodes
     mean_100ep_reward = np.mean(episode_rewards[-100:])
     print("Mean reward:", mean_100ep_reward, "Num episodes:", len(episode_rewards))
+    print(f"Success Rate: {done_events.count(1)/len(done_events)}")
+    print(f"Collision Rate: {done_events.count(-1) / len(done_events)}")
 
     return mean_100ep_reward
 
