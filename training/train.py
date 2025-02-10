2f7efa766f794e322346286c728376ef743d190c
RaikoPipe <eclipsedust@gmail.com>
2025-02-10 09:24:09 +0100

    - prep next run

--- training/train.py
+++ training/train.py
@@ -113,8 +113,8 @@ if __name__ == "__main__":
 
     hyperparams = Hyperparameters(algorithm=args.algorithm)
     hyperparams.batch_size = args.batch_size
-    hyperparams.policy_kwargs = dict(log_std_init=-3, net_arch=dict(pi=[128], qf=[256, 256]), optimizer_class=optax.adamw)
-    hyperparams.gradient_steps = 8
+    #hyperparams.policy_kwargs = dict(log_std_init=-3, net_arch=dict(pi=[128], qf=[256, 256]), optimizer_class=optax.adamw)
+    hyperparams.gradient_steps = 20
     #hyperparams.buffer_size = 300_000
 
     replay_buffer_class = HerReplayBuffer if args.replay_buffer_class == "HerReplayBuffer" else DictReplayBuffer
@@ -150,7 +150,7 @@ if __name__ == "__main__":
         train_config.success_thresholds = [1.0]
         train_config.ee_error_thresholds = [0.05]
         train_config.max_ep_steps = [200]
-    train_config.policy_type = "SimbaPolicy"
+    #train_config.policy_type = "SimbaPolicy"
     train_model(seeds=args.seeds, configs=[train_config], pretrained_model_name=args.pretrained_model)
 
     # Configure buffer size based on available memory
